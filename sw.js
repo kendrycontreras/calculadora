130d9003ba34724cda6ec1bc2223423e0be72a54
kendrycontreras <85978202+kendrycontreras@users.noreply.github.com>
2026-07-06 22:52:32 -0400

    Refactor service worker for asset caching

--- sw.js
+++ sw.js
@@ -1,10 +1,13 @@
 const CACHE_NAME = 'caja-pastelitos-v1';
-const ASSETS = ['./', './index.html'];
+// Quitamos los iconos locales de aquí, ya que los llamaremos por URL
+const ASSETS = ['./', './index.html']; 
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
